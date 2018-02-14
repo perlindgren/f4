527338f511628e9812072e30d3f167190d4f2fee
Travis CI User <travis@example.org>
2018-02-14 03:03:03 +0000

    Update documentation

---
+++ implementors/bare_metal/trait.Nr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["stm32f40x"] = ["impl <a class=\"trait\" href=\"bare_metal/trait.Nr.html\" title=\"trait bare_metal::Nr\">Nr</a> for <a class=\"enum\" href=\"stm32f40x/enum.Interrupt.html\" title=\"enum stm32f40x::Interrupt\">Interrupt</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["either"] = ["impl&lt;L:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>, R:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"either/enum.Either.html\" title=\"enum either::Either\">Either</a>&lt;L, R&gt;",];
+implementors["f4"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"f4/frequency/ahb1/struct.Ticks.html\" title=\"struct f4::frequency::ahb1::Ticks\">Ticks</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"f4/frequency/ahb2/struct.Ticks.html\" title=\"struct f4::frequency::ahb2::Ticks\">Ticks</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"f4/frequency/apb1/struct.Ticks.html\" title=\"struct f4::frequency::apb1::Ticks\">Ticks</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"f4/frequency/apb2/struct.Ticks.html\" title=\"struct f4::frequency::apb2::Ticks\">Ticks</a>",];
+implementors["nb"] = ["impl&lt;E:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"nb/enum.Error.html\" title=\"enum nb::Error\">Error</a>&lt;E&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
